60c898f7b1283ec6f1e5ff8a7c93a2d9cc6a2f5c
Henrick Adrian <henrick.adrian4@gmail.com>
2025-04-27 23:44:21 -0300

    Controller de registro de clientes.

---
+++ Controllers/RegistroClientesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SistemaDeRelatorioDeVenda.Data;
+using SistemaDeRelatorioDeVenda.Models;
+
+namespace SistemaDeRelatorioDeVenda.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RegistroClientesController : ControllerBase
+    {
+        private readonly ApiDbContext _context;
+        public RegistroClientesController(ApiDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        [Route("consultar-clientes")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<IEnumerable<Cliente>>> ConsultarClientes()
+        {
+            var clientes = await _context.Clientes.ToListAsync();
+            if (clientes == null || !clientes.Any())
+            {
+                return NotFound("Nenhum cliente encontrado.");
+            }
+            return Ok(clientes);
+        }
+
+    }
+}
